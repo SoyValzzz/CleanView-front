d892cc3a8433f38a9922850955cd2730231d42e6
valentino2805 <104285850+valentino2805@users.noreply.github.com>
2025-07-09 15:53:21 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   serverBasePath: "https://backend-web-applications-production-0747.up.railway.app/api/v1",
   apiBaseUrl: 'https://backend-web-applications-production-0747.up.railway.app/api/v1',
-  zoneEndPointPath: "/zones",
+  zoneEndPointPath: "/stores",
   wasteEndPointPath: "/wastes",
   sensorEndPointPath: "/sensors",
   actionEndPointPath: "/actions"
